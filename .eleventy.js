1816d4af7f93b12e4b7d163c4fe672e3ab5a546d
Morgane Mahé <maeamahe@gmail.com>
2023-04-11 18:28:18 +0200

    #7: create and config .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+/* Config file */
+module.exports = function(eleventyConfig) {
+
+    // Ce dossier est cloné lors du build
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/css");
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+};
